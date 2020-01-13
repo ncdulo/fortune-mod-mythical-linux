a62bba0937e9b3d5ca954b531fb3f063e71627ad
Ash <44369810+Phate6660@users.noreply.github.com>
2020-01-13 17:00:31 -0500

    tidy up more
    moar optimization, moar of muh minimalism
    down with bloated scripts!

--- install.sh
+++ install.sh
@@ -6,24 +6,16 @@ echo "Running STRFile on offensive fortunes..."
 strfile ./off/mythical_linux
 if test -d /usr/share/fortune; then
     echo "Copying normal fortunes to /usr/share/fortune..."
-    sudo cp ./mythical_linux /usr/share/fortune
-    echo "Copying normal fortune data to /usr/share/fortune..."
-    sudo cp ./mythical_linux.dat /usr/share/fortune
+    sudo cp mythical_linux{,.dat} /usr/share/fortune
     echo "Copying offensive fortunes to /usr/share/fortune/off..."
-    sudo cp ./off/mythical_linux /usr/share/fortune/off
-    echo "Copying offensive fortune data to /usr/share/fortune/off..."
-    sudo cp ./off/mythical_linux.dat /usr/share/fortune/off
+    sudo cp off/mythical_linux{,.dat} /usr/share/fortune/off
     echo "Permission-fixing fortunes at /usr/share/fortune..."
     sudo chmod 777 -R /usr/share/fortune
 elif test -d /usr/share/fortunes; then
     echo "Copying normal fortunes to /usr/share/fortunes..."
-    sudo cp ./mythical_linux /usr/share/fortunes
-    echo "Copying normal fortune data to /usr/share/fortunes..."
-    sudo cp ./mythical_linux.dat /usr/share/fortunes
+    sudo cp mythical_linux{,.dat} /usr/share/fortunes
     echo "Copying offensive fortunes to /usr/share/fortunes/off..."
-    sudo cp ./off/mythical_linux /usr/share/fortunes/off
-    echo "Copying offensive fortune data to /usr/share/fortunes/off..."
-    sudo cp ./off/mythical_linux.dat /usr/share/fortunes/off
+    sudo cp off/mythical_linux{,.dat} /usr/share/fortunes/off
     echo "Permission-fixing fortunes at /usr/share/fortunes..."
     sudo chmod 777 -R /usr/share/fortunes
 fi
